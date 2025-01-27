116b726ee14a5171ca677dc4ff325e305095b5cc
Jiyun-Juli-Kim <juliyooni@gmail.com>
2025-01-27 23:38:30 +0900

    250127 23:38 저장

--- VR Timpani Simulator/Assets/Scripts/AnswerRecorder.cs
+++ VR Timpani Simulator/Assets/Scripts/AnswerRecorder.cs
@@ -1,18 +1,19 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AnswerRecorder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    /*
+     1. 일단은, 시작 버튼이 있어야 함
+      1) 시작버튼 누르면 음원 재생 & 타이머로 시간 재기 시작
+     2. 인풋을 기록하는 리스트가 있어야 함
+      1) 기록하는 객체는 리스트로 할 것임
+      2) Note 클래스를 기록할건데, 
+       - 키의 종류에 따라 NotePitch, NoteType 받아오기
+       - 눌린 타이밍을 기록해서 NoteTime으로 저장
+       - 순서를 확인하기 위한 인덱스도 일단은 생성 하는 것으로 하자.  
+     3. 이 리스트를 CSV로 내보내면 끝
+      1) 클래스의 필드를 그대로 차례로 내보내면 됨
+     */
 }
